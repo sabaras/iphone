69f128b8986157e24b08099d6cc8cf3ff351a298
sabmgv <saba.mgvdeladze777@gmail.com>
2022-02-11 22:09:42 +0400

    Add iphone

---
+++ steve/steve/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace steve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("saba steve");
+        }
+    }
+}
